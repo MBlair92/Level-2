e1b28e1637569a1b3232b2c23de5f39ee4b92fbc
mercedes blair <blairduncan12@gmail.com>
2023-05-01 13:19:57 -0700

    multiple-array-methods-part1

---
+++ multiple-array-methods-part1/main.js
@@ -0,0 +1,60 @@
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+function getAdultsList(persons) {
+    const htmlList = persons
+      .filter(person => person.age > 18)
+      .sort((a, b) => a.lastName.localeCompare(b.lastName))
+      .map(person => `<li>${person.firstName} ${person.lastName}, ${person.age}</li>`);
+    return htmlList;
+  }
+  
+  const persons = [
+    { firstName: 'Sarah', lastName: 'Palin', age: 47 },
+    { firstName: 'Frank', lastName: 'Zappa', age: 12 },
+    { firstName: 'Rick', lastName: 'Sanchez', age: 78 },
+    { firstName: 'Morty', lastName: 'Smith', age: 29 },
+    { firstName: 'Kyle', lastName: 'Mooney', age: 27 },
+    { firstName: 'Pasha', lastName: 'Datsyuk', age: 13 },
+    { firstName: 'Lev', lastName: 'Tolstoy', age: 82 },
+  ];
+  
+  const adultsList = getAdultsList(persons);
+  
+  console.log(adultsList.join('\n'));
+    
