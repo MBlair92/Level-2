f0cbeb42a5b2e21f696a04a6ecc0ae9d17c13b96
mercedes blair <blairduncan12@gmail.com>
2023-08-02 13:51:20 -0700

    jsons-deli

---
+++ jsons-deli/menu.js
@@ -0,0 +1,57 @@
+{
+	"appetizers": [{
+			"name": "Hummus",
+			"ingredients": ["Spices", "Chickpeas"],
+			"description": "Chickpeas blended with mild spices, served with a side of programming code sauce."
+		},
+		{
+			"name": "Salad",
+			"ingredients": ["Lettus", "Tomato", "cucumber", "French Dressing", "Cheese"],
+			"description": "A fresh salad made with tomatoes, onions, crispy cucumbers, and vegan cheese,."
+		}
+	],
+	"cold sandwiches": [{
+			"name": "JSON's Club",
+			"ingredients": ["Turkey", "Bacon", "Lettuce", "Tomato", "Mayo", "Multigrain Bread"],
+			"description": "A stacked club sandwich featuring layers of turkey, bacon, lettuce, and tomato, served on wholesome multigrain bread."
+		},
+		{
+			"name": "Veggie Coder",
+			"ingredients": ["Avocado", "Cucumber", "Sprouts", "Hummus", "Whole Wheat Bread"],
+			"description": "A refreshing veggie sandwich packed with avocado, cucumber, and sprouts, spread with hummus, on whole wheat bread."
+		}
+	],
+	"hot sandwiches": [{
+			"name": "Vegan Burger",
+			"ingredients": ["Plant Based Patty", "Vegan Cheese", "Brioche Bun", "Special Sauce"],
+			"description": "A juicy plant based patty topped with melted vgean cheese and served on a soft brioche bun with our special sauce."
+		},
+		{
+			"name": "Grilled Mushrom Melt",
+			"ingredients": ["Grilled Mushrom", "Mozzarella", "Pita Bread", "Marinara Sauce"],
+			"description": "Made with Fresh grilled mushroms, melted mozzarella, marinara sauce, all placed into a warm pita bread."
+		}
+	],
+	"desserts": [{
+			"name": "Banana Cookies",
+			"ingredients": ["Eggs", "Butter", "Flour", " Vanilla Wafers", "Banana's"],
+			"description": "Freshly baked banana pudding cookies, made with fresh bananas vanilla wafers cookies, and topped with a sweet drizzel."
+		},
+		{
+			"name": "Apple Cheese cake",
+			"ingredients": ["Caramel", "Cream Cheese", "Heavy Whipped Cream", "Apple Crunch"],
+			"description": "A tasty layered cheese cake jar with caramel and a sweet apple crunch toping."
+		}
+	],
+	"drinks": [{
+			"name": "Lemonade",
+			"ingredients": ["Lemons", "Water", "Sugar"],
+			"description": "A refreshing lemonade drink made with water, a freash squeezd lemon, and sugar."
+		},
+		{
+			"name": "Specialty Lemonade",
+			"ingredients": ["Water", "Lemons", "Sugar", "Flavored Syrup", "Freash Fruit"],
+			"description": "A refreshing lemonade drink made with water, a freash squeezd lemon, freash fruit, suagr, and flavored syrup."
+		}
+	]
+}
